914d7a506802d4df1544bcd388a299d6195ad414
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2025-03-20 21:00:18 -0300

    config error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((err, _, reply) => {
+  if (err instanceof ZodError) {
+    return reply.status(400).send({ message: err.message, issues: err.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(err)
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
+  return reply.status(500).send('Internal server error')
+})
